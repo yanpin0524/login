bb45733ad0c771e76504f175080975ca2baf6c39
Leo <a131234568@gmail.com>
2022-03-18 20:19:30 +0800

    登入頁面

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -2,7 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  
+  res.render('login')
+})
+
+router.post('/',(req, res) => {
+  console.log(req.body)
 })
 
 module.exports = router
