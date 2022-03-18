4b43581dd3a952411750888106819f7dfc0091fa
Leo <a131234568@gmail.com>
2022-03-18 20:49:47 +0800

    新增登入功能

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,12 +1,25 @@
 const express = require('express')
 const router = express.Router()
+const Login = require('../../models/login')
 
 router.get('/', (req, res) => {
   res.render('login')
 })
 
-router.post('/',(req, res) => {
-  console.log(req.body)
+router.post('/', (req, res) => {
+  const body = req.body
+
+  Login.findOne({ email: body.email })
+    .lean()
+    .then((item) => {
+      if (!item) { return res.render('login', { loginFail: true }) }
+
+      if (item.password === body.password) {
+        return res.render('index', { firstName: item.firstName})
+      } else {
+        return res.render('login', { loginFail: true })
+      }
+    })
 })
 
 module.exports = router
